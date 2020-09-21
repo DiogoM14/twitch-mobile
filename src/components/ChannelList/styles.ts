fb1247092dba7a5018e040b84e597f348fe42837
Xavi002 <diogomartins200214@gmail.com>
2020-09-21 23:12:26 +0100

    UPDATED: styling offline channels

--- src/components/ChannelList/styles.ts
+++ src/components/ChannelList/styles.ts
@@ -1,20 +1,55 @@
 import styled from 'styled-components/native';
+import colors from '../../styles/colors';
 
-export const List = styled.View``;
+export const List = styled.View`
+  padding: 8px 0 0;
 
-export const ChannelContainer = styled.TouchableOpacity``;
+`;
 
-export const LeftSide = styled.View``;
+export const ChannelContainer = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-right: 14px;
 
-export const Avatar = styled.View``;
+  margin-bottom: 25px;
+`;
 
-export const Column = styled.View``;
+export const LeftSide = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
 
-export const Username = styled.Text``;
+export const Avatar = styled.View`
+  background: ${colors.tag};
+  width: 48px;
+  height: 48px;
+  border-radius: 24px;
+`;
 
-export const Info = styled.Text``;
+export const Column = styled.View`
+  padding-left: 10px;
+`;
+
+export const Username = styled.Text`
+  color: ${colors.white};
+  font-family: roboto_500;
+  font-size: 16px;
+`;
+
+export const Info = styled.Text`
+  margin-top: 1px;
+  color: ${colors.gray};
+  font-size: 13px;
+`;
 
 export const RightSide = styled.View``;
 
-export const WhiteCircle = styled.View``;
+export const WhiteCircle = styled.View`
+  background: ${colors.white};
+  width: 9px;
+  height: 9px;
+  border-radius: 4.5px;
+  opacity: .85;
+`;
 
