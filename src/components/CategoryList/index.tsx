0305b07e91c7e862759705a0b58a3661b80737e5
Xavi002 <diogomartins200214@gmail.com>
2020-09-21 11:59:01 +0100

    ADDED: following page/ header/ category list

---
+++ src/components/CategoryList/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+import { 
+  CategoryContainer,
+  CategoryImage,
+  CategoryName,
+  CategoryStatus,
+  RedCircle,
+  Info,
+  List
+ } from './styles';
+
+import data from './data';
+
+interface ItemProps {
+  item: typeof data[0];
+}
+
+const CategoryList: React.FC = () => {
+  const CategoryItem: React.FC<ItemProps> = ({ item }) => (
+    <CategoryContainer>
+      <CategoryImage source={item.source} />
+      <CategoryName numberOfLines={1}>{item.name}</CategoryName>
+      <CategoryStatus>
+        <RedCircle />
+        <Info>12.2k</Info>
+      </CategoryStatus>
+    </CategoryContainer>
+  )
+
+  return (
+    <List>
+      {data.map((item) => (
+        <CategoryItem key={item.name} item={item} />
+      ))}
+    </List>
+  );
+};
+
+export default CategoryList;
